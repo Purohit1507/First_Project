ec894d8eeec9c82050aa8af76b9f6751eb4c1ff1
Parth Purohit <purohitparth1@gmail.com>
2023-03-21 06:00:04 +0530

    Create Tesr1.java

---
+++ Tesr1.java
@@ -0,0 +1,14 @@
+package Basic_Java;
+
+public class Tesr1 {
+
+	public static void main(String[] args) {
+		int a=25;
+		int b=30;
+		System.out.println("summation"+"="+(a+b));
+		System.out.println("Substraction"+"="+(a-b));
+		System.out.println("division"+"="+(float)a/b);
+		System.out.println("multiplication"+"="+(a*b));
+	}
+
+}
